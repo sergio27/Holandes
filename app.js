8f191f0aa357c69a1e0bc450ec91c2f01d1e8d6a
Sergio <ibarra.sergio@gmail.com>
2021-03-10 00:01:13 -0700

    Agrega puntuación a quiz

--- app.js
+++ app.js
@@ -24,39 +24,12 @@ app.get("/", function(req, res) {
           }
       });
 
-      res.render("index", {words: allWords, categories: categories});
+      res.render("index", {words: allWords, categories: categories.sort()});
   })
 });
 
 app.get("/:category", function(req, res) {
-
-  fs.readFile('./public/docs/words.json', 'utf8', (err, jsonString) => {
-      if (err) {
-          console.log("File read failed:", err)
-          return
-      }
-
-      const allWords = JSON.parse(jsonString);
-      const words = [];
-
-      allWords.forEach(function(word) {
-          if(req.params.category == word.category) {
-            words.push(word);
-          }
-      });
-
-      words.sort(function(a, b) {
-        if (a.dutch < b.dutch) {
-          return -1;
-        }
-        if (a.dutch > b.dutch) {
-          return 1;
-        }
-        return 0;
-      });
-
-      res.render("category", {words: words, category: req.params.category});
-  })
+      res.render("category", {category: req.params.category});
 });
 
 app.get("/:category/Vocabulario", function(req, res) {
--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,6 +1,9 @@
 const words = [];
 const category = $(".lbl-category").text();
 
+let wordCount = 0;
+let points = 0;
+
 $.getJSON('../../docs/words.json', function(data) {
     data.forEach(function(word) {
       if (category == word.category) {
@@ -8,26 +11,52 @@ $.getJSON('../../docs/words.json', function(data) {
       }
     });
 
+    shuffleArray(words);
     nextWord();
 });
 
-$(".btn-next").click(function() {
+$('.txt-answer').keypress(function (e) {
+  if (e.which == 13) {
+    btnNextClick();
+    return false;    //<---- Add this line
+  }
+});
+
+$(".btn-next").click(btnNextClick);
+
+function btnNextClick() {
   if($(".btn-next").text() == "Comprobar") {
     checkWord();
-    $(".btn-next").text("Siguiente");
+
+    if(wordCount < words.length) {
+      $(".btn-next").text("Siguiente");
+    }
+    else {
+      let texto = "Resultado: " +points +" de " +words.length +" palabras correctas.";
+      $(".lbl-wordcount").text(texto);
+
+      $(".btn-next").text("Reiniciar");
+    }
+  }
+  else if($(".btn-next").text() == "Siguiente") {
+      nextWord();
+      $(".btn-next").text("Comprobar");
   }
   else {
+    wordCount = 0;
+    points = 0;
+    shuffleArray(words);
     nextWord();
+
     $(".btn-next").text("Comprobar");
   }
-});
+}
 
 function checkWord() {
-
   let answer = $(".txt-answer").val();
-  console.log(answer);
 
   if($(".fc-spanish").text() == answer) {
+    points++;
     $(".fc-spanish").removeClass("btn-danger").addClass("btn-success");
   }
   else {
@@ -38,11 +67,21 @@ function checkWord() {
 }
 
 function nextWord() {
-  let random = Math.floor(Math.random() * Math.floor(words.length));
-
-  $(".fc-dutch").text(words[random].dutch);
-  $(".fc-spanish").text(words[random].spanish);
+  $(".fc-dutch").text(words[wordCount].dutch);
+  $(".fc-spanish").text(words[wordCount].spanish);
 
   $(".txt-answer").val("");
   $(".fc-spanish").hide();
+
+  wordCount++;
+
+  let texto = "Palabra " +wordCount +" de " +words.length +".";
+  $(".lbl-wordcount").text(texto);
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
